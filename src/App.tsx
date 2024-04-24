a26003d1bee91f4cbb74a69020a94249f7857928
Ritwick Sv <43278208+RitwickSV@users.noreply.github.com>
2024-04-24 13:17:48 +0530

    Added Logo in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    // <Grid templateAreas={`"nav nav" "sidebar main"`}>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "sidebar main"`,
+      }}
+    >
+      <GridItem area="nav" bg="papayawhip">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="sidebar" bg="dodgerblue">
+          Sidebar
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="coral">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
